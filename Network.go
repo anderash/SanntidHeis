c9bd8d3757fd447dc8793ca85031afe07e56d385
Anders Åsheim <anders.aasheim@hotmail.com>
2015-02-06 12:03:18 +0100

    Udp broadcast funksjon lagt til

--- Network.go
+++ Network.go
@@ -1,16 +1,39 @@
 // NETWORK MODULE //
+package network
 
 import (
   "fmt"
   "net"
   "time"
+  "os"
 )
 
 constant (
 	OwnIP := "129.241.187.140"
 	OwnPort := "20001"
+	Baddr := "129.241.187.255"
 )
 
-func UDPBroadcast() {
+func UDPBroadcast(data str) {
+	buffer := make([]byte(data))
+
+	raddr, err1 := net.resolveUDPaddr("udp", Baddr+":"+OwnPort)
+
+		if err != nil {
+		fmt.Printf("Addresse dritt")
+		os.Exit(1)
+		}
+
+	socket, err2 := net.DialUDP ("udp", nil, raddr)
+
+	n, _ = socket.Write(buffer)
+
 	
+
+}
+
+func UDPListen(){
+	buffer := make([]byte)
+
+
 }
