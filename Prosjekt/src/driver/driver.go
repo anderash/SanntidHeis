b1698657a65b3d75aa04d1bc31b227feae2267ce
root <root@itk-d651>
2015-03-04 11:00:18 +0100

    Alle get og set funksjoner funker

--- Prosjekt/src/driver/driver.go
+++ Prosjekt/src/driver/driver.go
@@ -13,45 +13,63 @@ import (
 
 // Denne bør helst endres til const!!
 var button_matrix = [N_FLOORS][3]int{
-	[3]int{BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1},
-	[3]int{BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2},
-	[3]int{BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3},
-	[3]int{BUTTON_UP4, BUTTON_DOWN4, BUTTON_COMMAND4}}
+	{BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1},
+	{BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2},
+	{BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3},
+	{BUTTON_UP4, BUTTON_DOWN4, BUTTON_COMMAND4}}
+
+var buttonlight_matrix = [N_FLOORS][3]int{
+	{LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
+	{LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
+	{LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
+	{LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4}}
+
 
 func Initiate() {
-	// Init hardware
-	C.io_init()
+	Io_init()
 
 	// Zero all floor button lamps
 	for i := 0; i < N_FLOORS; i++ {
-		for j := 0; j < 3; j++ {
-			Set_button_lamp(button_matrix[i][j], 0)
+		if i != 0{
+			Set_button_lamp(BUTTON_CALL_DOWN, i, 0)
 		}
+		if i != (N_FLOORS - 1) {
+			Set_button_lamp(BUTTON_CALL_UP, i, 0)
+		}
+		Set_button_lamp(BUTTON_COMMAND, i, 0)
 	}
+
+	// Zero door open lamp
+	Set_door_open_lamp(0)
+
+	// Make sure motor is dead
+	Set_motor_direction(0)
+
+
 
 	fmt.Printf("Initiated!\n")
 }
 
 // Funker
 func Get_floor_signal() int {
-	if C.io_read_bit(C.int(SENSOR_FLOOR1)) == 1 {
+	if Io_read_bit(SENSOR_FLOOR1) == 1 {
 		return 0
-	} else if C.io_read_bit(C.int(SENSOR_FLOOR2)) == 1 {
+	} else if Io_read_bit(SENSOR_FLOOR2) == 1 {
 		return 1
-	} else if C.io_read_bit(C.int(SENSOR_FLOOR3)) == 1 {
+	} else if Io_read_bit(SENSOR_FLOOR3) == 1 {
 		return 2
-	} else if C.io_read_bit(C.int(SENSOR_FLOOR4)) == 1{
+	} else if Io_read_bit(SENSOR_FLOOR4) == 1{
 		return 3
 	} else {
 		return -1
 	}
 }
 
-// Utestet
+// Funker
 func Get_button_signal() (int, int) {
 	for i := 0; i < N_FLOORS; i++ {
 		for j := 0; j < 3; j++ {
-			if button_matrix[i][j] != -1 && C.io_read_bit(C.int(button_matrix[i][j]) == 1){
+			if (button_matrix[i][j] != -1) && (Io_read_bit(button_matrix[i][j]) == 1){
 				return i, j
 				// i tilsvarer etage (0 = 1. etg, 1 = 2. etg. osv)
 				// j tilsvarer type knapp. (0 = opp-knapp, 1 = ned-knapp, 2 = knapp inne i heis)
@@ -65,40 +83,54 @@ func Get_button_signal() (int, int) {
 
 }
 
-// Noe rart. Tenner flere lamper enn det den skal
-func Set_button_lamp(button int, value int) {
-	if button == -1 {
-		return
-	}
+// Funker
+func Set_button_lamp(button int, floor int, value int) {
 	if value == 1 {
-		C.io_set_bit(C.int(button))
+		Io_set_bit(buttonlight_matrix[floor][button])
 	} else {
-		C.io_clear_bit(C.int(button))
+		Io_clear_bit(buttonlight_matrix[floor][button])
 	}
 }
 
-// Noe rart. Tenner flere lamper enn det den skal
+// Funker
 func Set_door_open_lamp(value int) {
 	if value == 1 {
-		C.io_set_bit(LIGHT_DOOR_OPEN)
+		Io_set_bit(LIGHT_DOOR_OPEN)
 	} else {
-		C.io_clear_bit(LIGHT_DOOR_OPEN)
+		Io_clear_bit(LIGHT_DOOR_OPEN)
 	}
 }
 
-// Funker!
+// Funker
+func Set_floor_indicator(floor int) {
+	// Passer her på at ett lys alltid er tent
+	if floor&0x02 == 0x02 {
+		Io_set_bit(LIGHT_FLOOR_IND1)
+	} else {
+		Io_clear_bit(LIGHT_FLOOR_IND1)
+	}
+	if floor&0x01 == 0x01 {
+		Io_set_bit(LIGHT_FLOOR_IND2)
+	} else {
+		Io_clear_bit(LIGHT_FLOOR_IND2)
+	}
+	
+}
+
+// Funker
 func Set_motor_direction(direction int) {
 	if direction == 0 {
-		C.io_write_analog(C.int(MOTOR), 0)
+		Io_write_analog(MOTOR, 0)
 	} else if direction > 0 {
-		C.io_clear_bit(C.int(MOTORDIR))
-		C.io_write_analog(C.int(MOTOR), 2800)
+		Io_clear_bit(MOTORDIR)
+		Io_write_analog(MOTOR, 2800)
 	} else if direction < 0 {
-		C.io_set_bit(MOTORDIR)
-		C.io_write_analog(MOTOR, 2800)
+		Io_set_bit(MOTORDIR)
+		Io_write_analog(MOTOR, 2800)
 	}
 
 }
+
 
 
 /*
--- Prosjekt/src/main2.go
+++ Prosjekt/src/main2.go
@@ -4,13 +4,20 @@ package main
 import(
 	"fmt"
 	"./driver"
-	//"time"
+	// "time"
 )
 
 func main() {
 	fmt.Printf("Starting driver\n")
 	driver.Initiate()
-	//driver.Set_button_lamp(driver.LIGHT_DOWN2,1)
-	//time.Sleep(1000 * time.Millisecond)
-	//driver.Set_door_open_lamp(1)
+	driver.Set_motor_direction(0)
+
+	// for {
+	// 	fmt.Println(driver.Get_button_signal())
+	// }
+	driver.Set_button_lamp(driver.BUTTON_CALL_DOWN, 1 ,1)
+	// driver.Set_door_open_lamp(1)
+	// time.Sleep(1000 * time.Millisecond)
+	// driver.Set_door_open_lamp(0)}
+
 }
