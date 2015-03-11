632ac5e30a1c81381d42c17694487a2680e1503a
Marty78 <martinhals@outlook.com>
2015-03-11 12:17:00 +0100

    Såvidt begynt på kostnadsfunksjon

--- Prosjekt/src/queue/queuemanager.go
+++ Prosjekt/src/queue/queuemanager.go
@@ -7,37 +7,61 @@ import(
 type  Elevator struct{
 	IPADDR string
 	POSITION int
-	DIRECTION int
-	ORDER_MATRIX [][]int
+	/*
+	   Etg.			Pos. nr.
+	    1 ............ 1
+	  	  ............ 2
+		2 ............ 3
+		  ............ 4
+		3 ............ 5
+		  ............ 6
+		4 ............ 7
+	*/
 
-	// 			inne_i_heis    ned       opp				Settes til 1 ved en ordre
-	// 1.etg	[[  0          0         0]
-	// 2.etg 	[   0          0         0]
-	// 3.etg 	[   0          0         0]
-	// 4.etg	[   0          0         0]]
+	DIRECTION int
+	/*
+		opp = 1, ned = -1, stillestående = 0 
+	*/
+
+	DESTINATION int
+
+	ORDER_MATRIX [][]int
+	/* 			inne_i_heis    ned       opp				Settes til 1 ved en ordre
+	   1.etg	[[  0          0         0]
+	   2.etg 	 [  0          0         0]
+	   3.etg 	 [  0          0         0]
+	   4.etg	 [  0          0         0]]
+	   osv.
+	*/
 }
 
+const(
+	N_FLOORS = 4
+ 	N_POSITIONS = N_FLOORS + (N_FLOORS-1)
+)
 
+// Indexen i map'en er ip-adressen til den aktuelle heisen
 var active_elevators =  make(map[string]Elevator)
 
 
+
 func InitQueuemanager(ipaddr string) {
-	my_ordermatrix := make([][]int, 4)
-	for i := 0; i < 4; i++{
+	my_ordermatrix := make([][]int, N_FLOORS)
+	for i := 0; i < N_FLOORS; i++{
 		my_ordermatrix[i] = []int{0,0,0}
 	}
-	new_elevator := Elevator{ipaddr, 0, 0, my_ordermatrix} 
+	new_elevator := Elevator{ipaddr, 0, 0, 0, my_ordermatrix} 
 	active_elevators[ipaddr] = new_elevator
 	fmt.Println("Elevator", active_elevators[ipaddr].IPADDR, "online")
 }
 
 
 func AppendElevator(ipaddr string) {
-	new_ordermatrix := make([][]int, 4)
-	for i := 0; i < 4; i++{
+	new_ordermatrix := make([][]int, N_FLOORS) 
+	for i := 0; i < N_FLOORS; i++{
 		new_ordermatrix[i] = []int{0,0,0}
 	}
-	new_elevator := Elevator{ipaddr, 0, 0, new_ordermatrix}
+	new_elevator := Elevator{ipaddr, 0, 0, 0, new_ordermatrix}
 	active_elevators[ipaddr] = new_elevator
 	fmt.Println("Elevator", active_elevators[ipaddr].IPADDR, "online")}
 
@@ -46,7 +70,7 @@ func AppendElevator(ipaddr string) {
 func PrintActiveElevators() {
 	for i := range(active_elevators){
 		fmt.Println("Elevator:",active_elevators[i].IPADDR)
-		for floor := 0; floor < 4; floor++ {	
+		for floor := 0; floor < N_FLOORS; floor++ {	
 			fmt.Println("Floor", floor + 1, ":", active_elevators[i].ORDER_MATRIX[floor])
 		}
 		fmt.Println("\n")	
@@ -58,4 +82,26 @@ func PrintActiveElevators() {
 func  RemoveElevator(ipaddr string) {
 	delete(active_elevators, ipaddr)
 	fmt.Println("Deleting", ipaddr, "\n")
+}
+
+func AppendOrder() {
+
+	
+}
+
+func costFunction(elevator_ip string, order_floor int, button_dir string) int{
+	cost := 0
+	current_elevator := active_elevators[elevator_ip]
+
+	//Omregner etg. nr. til posisjonsnr. (Ihht. structen Elevator)
+	order_floor_pos := order_floor + (order_floor - 1)
+	dest_pos := current_elevator.DESTINATION + (current_elevator.DESTINATION - 1)
+
+	// Sjekker alle utfall hvor bestillingsknapp "opp" er trykket
+	if button_dir == "up" {
+		
+		if  current_elevator.DIRECTION == 1 && dest_pos >=  order_floor_pos {
+			cost = dest_pos - current_elevator.POSITION
+		}
+	}
 }
