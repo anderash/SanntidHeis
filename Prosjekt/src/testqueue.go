1932d88e47601d207c7316611174f17d3a84a3d4
Martin <marthals@stud.ntnu.no>
2015-03-13 13:10:08 +0100

    Fungerende kostfunksjon

--- Prosjekt/src/testqueue.go
+++ Prosjekt/src/testqueue.go
@@ -1,18 +1,27 @@
 package main
 
-import(
+import (
 	"./queue"
-	// "fmt"
+	"fmt"
 )
 
 func main() {
 	ipaddr := "123.45.345"
+	position := 4
+	direction := 0
+	destination_floor := 3
+	order_floor := 2
+	button_dir := "down"
+
 	queue.InitQueuemanager(ipaddr)
-	queue.AppendElevator("999.99.999")
+
+	queue.PrintActiveElevators()
+	queue.SetElevator(ipaddr, position, direction, destination_floor)
+
 	queue.PrintActiveElevators()
 
-	queue.RemoveElevator(ipaddr)
-	queue.PrintActiveElevators()
-
+	// queue.RemoveElevator(ipaddr)
+	// queue.PrintActiveElevators()
+	fmt.Println("Cost:", queue.CostFunction(ipaddr, order_floor, button_dir))
 
 }
