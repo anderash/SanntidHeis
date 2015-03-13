4cc87742d5b82cd3792f112b3230a4073054df2d
Anders Åsheim <anders.aasheim@hotmail.com>
2015-03-13 11:02:18 +0100

    noen syntaxrettelser

--- Prosjekt/src/elevManager/elevManager.go
+++ Prosjekt/src/elevManager/elevManager.go
@@ -1,8 +1,8 @@
 package elevManager
 
-import {
+import (
 	"fmt"
-}
+)
 
 /* Denne skal opprette ny goroutine for ny heis.
 Hver routine har en for select for enten oppdatert info, eller timeout.
@@ -12,70 +12,65 @@ Husk å også drepe goroutine
 
 */
 
-type ElevInfo {
-	IPADDR string
+type ElevInfo struct {
+	IPADDR     string
 	F_NEW_INFO bool
 
 	F_DEAD_ELEV bool
 
-	POSITION int
-	DIRECTION int
+	POSITION    int
+	DIRECTION   int
 	DESTINATION int
 
-	ButtonType int
+	ButtonType  int
 	ButtonFloor int
 }
 
-
-
 func InitBank(c_from_main chan []byte) {
-	
+
 	c_to_queuemanager := make(chan []byte)
 	var info_package ElevInfo
 	var info_to_quemanager queueModuleInfo
 
 	bank := make(map[string]ElevInfo)
 
-	for{
-		select{
-			case: from_main <- c_from_main{
-				json_err := json.Unmarshal(from_main, &info_package)
-				if json_err != nil {
-					fmt.Println("error: ", err)
-				}
-
-				elev, in_bank := bank[info_package.IPADDR]
-				if (!in_bank) {
-					c_newchannel := make(chan bool)
-					go spawnElevcheck(c_newchannel)
-
-
-					c_to_queuemanager <- NyHeis
-
-				} else if (ElevInfo.FLAG_NEW_INFO == true){
-					denDetGjelderSinKanal <- DuLever
-					c_Kømodul <- NyInfo
-
-				} else{
-					// Kun fått alive-melding, send på den det gjelder sin kanal at du lever
-					denDetGjelderSinKanal <- DuLever
-				}
-
+	for {
+		select {
+		case from_main <- c_from_main:
+			json_err := json.Unmarshal(from_main, &info_package)
+			if json_err != nil {
+				fmt.Println("error: ", err)
 			}
+
+			elev, in_bank := bank[info_package.IPADDR]
+			if !in_bank {
+				c_newchannel := make(chan bool)
+				go spawnElevcheck(c_newchannel)
+
+				c_to_queuemanager <- info_package
+
+			} else if ElevInfo.FLAG_NEW_INFO == true {
+				denDetGjelderSinKanal <- DuLever
+				c_Kømodul <- NyInfo
+
+			} else {
+				// Kun fått alive-melding, send på den det gjelder sin kanal at du lever
+				denDetGjelderSinKanal <- DuLever
+			}
+
 		}
 	}
 }
 
-
 func spawnElevcheck(minKanal) {
-	for{
-		select{
-			case: <- minKanal
-				//Jeg er i live
-			case: <- timeout
-				//Jeg døde
-				 c_kømodul <- JegDøde
-				 return
+	for {
+		select {
+		case <-minKanal:
+			//Jeg er i live
+		case <-timeout:
+			//Jeg døde
+			c_kømodul <- JegDøde
+			return
 		}
-	} 
+	}
 }
--- Prosjekt/src/testElevManager.go
+++ Prosjekt/src/testElevManager.go
@@ -1,32 +1,31 @@
 package main
 
-import{
-	"fmt"
+import (
 	"./elevManager"
 	"encoding/json"
+	"fmt"
 	"runtime"
-}
+)
 
-type ElevInfo {
-	IPADDR string
+type ElevInfo struct {
+	IPADDR     string
 	F_NEW_INFO bool
 
 	F_DEAD_ELEV bool
 
-	POSITION int
-	DIRECTION int
+	POSITION    int
+	DIRECTION   int
 	DESTINATION int
 
-	ButtonType int
+	ButtonType  int
 	ButtonFloor int
 }
 
 func main() {
 	infoPackage := make(chan []byte)
-	testMelding := main_info{"123.456.789", true, 3, -1, 1, 1, 1}
+	testMelding := main_info{"123.456.789", true, false, 3, -1, 1, 1, 1}
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	go InitBank(infoPackage)
 
-	
 }
