b35af34106735dfaa44d27280af939cddaddeb74
Anders Åsheim <anders.aasheim@hotmail.com>
2015-04-12 15:04:59 +0200

    ryddet litt canalnavn

--- Prosjekt/src/testNetToQueue.go
+++ Prosjekt/src/testNetToQueue.go
@@ -16,19 +16,19 @@ func main() {
 
 //	c_mainchannel := make(chan []byte)
 	c_peerUpdate := make(chan string)
-	c_to_queuemanager := make(chan []byte)
+	c_queMan_info := make(chan []byte)
 
 	ipaddr := network.GetOwnIP()
-	c_to_statemachine := make(chan int)
-	c_pos_from_statemachine := make(chan int)
-	c_dir_from_statemachine := make(chan int)
+	c_queMan_destination := make(chan int)
+	c_SM_position := make(chan int)
+	c_SM_direction := make(chan int)
 
 	c_toNetwork := make(chan []byte)
 	c_fromNetwork := make(chan []byte)
 
 	go network.UDPNetwork(c_toNetwork, c_fromNetwork, c_peerUpdate)
-	go elevManager.InitBank(c_fromNetwork, c_peerUpdate, c_to_queuemanager)
-	queue.InitQueuemanager(ipaddr, c_to_queuemanager, c_to_statemachine, c_pos_from_statemachine, c_dir_from_statemachine)
+	go elevManager.InitBank(c_fromNetwork, c_peerUpdate, c_queMan_info)
+	queue.InitQueuemanager(ipaddr, c_queMan_info, c_queMan_destination, c_SM_position, c_SM_direction)
 
 	go AliveRoutine(ipaddr, c_toNetwork)
 
