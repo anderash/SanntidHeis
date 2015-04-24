07ac4958ca69dcb06b017748ff5d6cbcd4da30d0
marty78 <marthals@stud.ntnu.no>
2015-04-24 17:08:03 +0200

    Tilbake til gamle endringer. Fikset så appendElevator() får oppdatert POS, DIR, DEST

--- Prosjekt/src/network/Network.go
+++ Prosjekt/src/network/Network.go
@@ -12,8 +12,8 @@ import (
 )
 
 const (
-	OwnPort       = "20003"
-	MsgPort       = "20003"
+	OwnPort       = "20018"
+	MsgPort       = "20018"
 	Baddr         = "129.241.187.255"
 	aliveInterval = 500 * time.Millisecond
 	deadTimeout   = 2 * time.Second
